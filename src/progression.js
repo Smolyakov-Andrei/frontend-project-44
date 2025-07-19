c48363e233d73145751dd126633c3c25c030c61b
Smolyakov-Andrei <Chrisztw@mail.ru>
2025-07-19 16:40:04 +0700

    13

--- src/progression.js
+++ src/progression.js
@@ -6,27 +6,25 @@ const playProgressionGame = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('What number is missing in the progression?');
+  
   const roundsCount = 3;
-  const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
+  const progressionLength = 10; 
 
   for (let i = 0; i < roundsCount; i++) {
     const start = Math.floor(Math.random() * 10) + 1;
-    const step = Math.floor(Math.random() *5) + 1;
-    const length = 10;
-    const progression = generateSequence(start, step, length);
+    const step = Math.floor(Math.random() * 5) + 1;
+    const progression = generateSequence(start, step, progressionLength);
     
-    const hiddenIndex = Math.floor(Math.random() * progression.length);
+ 
+    const hiddenIndex = Math.floor(Math.random() * (progressionLength - 10)) + 5;
     const correctAnswer = progression[hiddenIndex].toString();
-
 
     const progressionWithHidden = [...progression];
     progressionWithHidden[hiddenIndex] = '..';
     const question = progressionWithHidden.join(' ');
 
-    console.log(`Questoin: ${question}`);
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    
 
     if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
@@ -36,7 +34,6 @@ const playProgressionGame = () => {
     console.log('Correct!');
   }
 
-  // Если все ответы верны
   console.log(`Congratulations, ${userName}!`);
 };
 
