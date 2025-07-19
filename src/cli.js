c42ba5eb80e9e1114564b493e6f9f0204e1be9a7
Smolyakov-Andrei <Chrisztw@mail.ru>
2025-07-19 13:08:10 +0700

    9

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 export default function greetUser() {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May i Have your name?');
+  const userName = readlineSync.question('May I Have your name?');
   console.log(`Hello, ${userName}!`);
 }
